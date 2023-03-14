4d99fcc0773b9a231fa3d3d15e7f2a275582552e
Nishant Kishore <ynm2@cdc.gov>
2023-03-14 15:31:03 -0400

    minor edits

--- R/hello_sir.R
+++ R/hello_sir.R
@@ -1,10 +1,13 @@
-
 #'Hello SIR Function
 #'
 #' @name hello_sir
 #' @description An example function
+#' @keywords sir
 #' @returns Hello world
 #' @export
+#' @examples
+#' hello_sir()
+
 hello_sir <- function(){
   print("Hello SIR!")
 }
