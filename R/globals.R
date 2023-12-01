b60d6c3762e3fdad097b836fd1322f818875ffbf
Heaghneyn <103440775+Heaghneyn@users.noreply.github.com>
2023-12-01 15:03:10 -0500

    add other surveillance to raw data pull

--- R/globals.R
+++ R/globals.R
@@ -19,7 +19,7 @@ utils::globalVariables(c(
   'dpt1', 'dpt3', 'epi.week', 'f.yrs.01', 'final.cell.culture.result', 'hot.case',
   'iris', 'lat', 'lon', 'mcv1', 'paralysis.asymmetric', 'paralysis.onset.fever', 'paralysis.rapid.progress',
   'person.sex', 'place.admin.0', 'place.admin.1','place.admin.2', 'surveillancetypename',
-  'under5_pop','yr.end','yronset', 'yr.st'
+  'under5_pop','yr.end','yronset', 'yr.st', 'name', 'lastModified', 'size'
 ))
 
 # "ADM0_NAME adequacy.final adm0guid adm1guid adm2guid afp.cases
