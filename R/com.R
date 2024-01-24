56029b77632e8acf66b7d327f285332263b7b0fd
Nishant Kishore <ynm2@cdc.gov>
2024-01-24 16:08:27 -0500

    :sparkle: added sharepoint upload fx closes #24

--- R/com.R
+++ R/com.R
@@ -20,3 +20,23 @@ send_teams_message <- function(msg, team_id = "CGH-GID-PEB-SIR", channel = "CORE
                        attachments = NULL)
 
 }
+
+#' Upload file to Sharepoint
+#'
+#' @description
+#' Helper function to upload file to MS Sharepoint
+#' @import Microsoft365R
+#' @param upload str: local path of files to be uploaded
+#' @param sp_folder str: location in sharepoint to upload file
+#' @param site str: Sharepoint site location, defaults to "CGH-GID-PEB"
+#' @param drive str: Sharepoint drive to upload data to
+#' @returns Success or error message
+upload_to_sharepoint <- function(upload, sp_folder, site = "CGH-GID-PEB-SIR", drive = "Documents"){
+
+  site <- get_sharepoint_site(site)
+
+  drv <- site$get_drive(drive)
+
+  drv$upload_file(src = upload, dest = sp_folder)
+
+}
