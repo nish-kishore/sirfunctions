77016eec2c7a0bb1eb05b2d576531b52c5346cce
Heaghneyn <103440775+Heaghneyn@users.noreply.github.com>
2024-01-26 17:51:56 -0500

    distinguish object type to add metadata tag

---
+++ R/f.metadata.tag.R
@@ -0,0 +1,16 @@
+#' @description add metadata tags to figures and tables
+#' @param object_type str: specify whether a "figure" or a "table"
+#' @param object obj: the figure or table to add metadata to
+#' @param metadata raw.data$metadata$download_time
+f.metadata.tag <- function(object_type,
+                           object,
+                           metadata = raw.data$metadata$download_time){
+
+  if(object_type == "figure"){
+
+  }
+
+  if(object_type == "table"){
+
+  }
+}
