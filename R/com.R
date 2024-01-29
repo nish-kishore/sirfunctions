2e21d1e6206646110e601e7ef81b5853a2e37769
Nishant Kishore <ynm2@cdc.gov>
2024-01-29 14:42:31 -0500

    :sparkle: modified sharepoint fx to refer to URL rather than name

--- R/com.R
+++ R/com.R
@@ -26,12 +26,12 @@ send_teams_message <- function(msg, team_id = "CGH-GID-PEB-SIR", channel = "CORE
 #' @description
 #' Helper function to upload file to MS Sharepoint
 #' @import Microsoft365R AzureAuth
-#' @param upload str: local path of files to be uploaded
-#' @param sp_folder str: location in sharepoint to upload file
-#' @param site str: Sharepoint site location, defaults to "CGH-GID-PEB"
+#' @param file_to_upload str: local path of files to be uploaded
+#' @param sharepoint_file_loc str: location in sharepoint to upload file
+#' @param site str: Sharepoint site location, defaults to "CGH-GID-PEB" or the site URL: "https://cdc.sharepoint.com/teams/CGH-GID-PEB-SIR283"
 #' @param drive str: Sharepoint drive to upload data to
 #' @returns Success or error message
-upload_to_sharepoint <- function(upload, sp_folder, site = "CGH-GID-PEB-SIR", drive = "Documents"){
+upload_to_sharepoint <- function(file_to_upload, sharepoint_file_loc, site = "https://cdc.sharepoint.com/teams/CGH-GID-PEB-SIR283", drive = "Documents"){
 
   tokens <- AzureAuth::list_azure_tokens()
 
@@ -51,19 +51,15 @@ upload_to_sharepoint <- function(upload, sp_folder, site = "CGH-GID-PEB-SIR", dr
     filter(grepl("Sites.ReadWrite.All", scope))
 
   if(nrow(token_list == 0)){
-    site <- Microsoft365R::get_sharepoint_site(site)
+    site <- Microsoft365R::get_sharepoint_site(site_url = site)
 
-    drv <- site$get_drive(drive)
-
-    drv$upload_file(src = upload, dest = sp_folder)
   }else{
-    site <- Microsoft365R::get_sharepoint_site(site, token = tokens[[pull(token_list, token)]])
+    site <- Microsoft365R::get_sharepoint_site(site_url = site, token = tokens[[pull(token_list, token)]])
 
-    drv <- site$get_drive(drive)
-
-    drv$upload_file(src = upload, dest = sp_folder)
   }
 
+  drv <- site$get_drive(drive)
 
+  drv$upload_file(src = file_to_upload, dest = sharepoint_file_loc)
 
 }
