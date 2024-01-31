06c22a7be8af0257e2fa456dc440db84ea8584ec
Heaghneyn <103440775+Heaghneyn@users.noreply.github.com>
2024-01-31 13:25:30 -0500

    remove negation on exists(raw.data)

--- R/f.metadata.tag.R
+++ R/f.metadata.tag.R
@@ -6,26 +6,27 @@
 f.metadata.tag <- function(object,
                            metadata = raw.data$metadata$download_time){
 
-  if(!exists(raw.data)){
-    stop()
-  }else{
+  object.class <- data.frame(class(object))
 
-  if(object_type == "figure"){
-    if(is.null(object$labels$caption)){
-      object$labels$caption <- paste0("Data downloaded from POLIS: ",
-                                      metadata)
-    }else{
-      object$labels$caption <- paste0(
-        object$labels$caption,
-        "\n",
-        "Data downloaded from POLIS: ",
-        metadata
-      )
+  if(exists("raw.data")){
+    if(grepl("ggplot", object.class$class.object.)){
+      if(is.null(object$labels$caption)){
+        object$labels$caption <- paste0("Data downloaded from POLIS: ",
+                                        metadata)
+      }else{
+        object$labels$caption <- paste0(
+          object$labels$caption,
+          "\n",
+          "Data downloaded from POLIS: ",
+          metadata
+        )
+      }
     }
-  }
-  if(object_type == "table"){
-    object <- flextable::add_footer_lines(object, paste0("Data Downloaded from POLIS: ", raw.data$metadata$download_time))
+    if(class(object) == "flextable"){
+      object <- flextable::add_footer_lines(object, paste0("Data Downloaded from POLIS: ", raw.data$metadata$download_time))
     }
+  }else{
+    stop()
   }
   return(object)
 }
