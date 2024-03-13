9b5eafda62148326879910c8d22e2ef801c47d2e
Nishant Kishore <ynm2@cdc.gov>
2024-03-13 04:42:55 -0400

    initialize parameters

---
+++ R/create_emergence_group_gif.R
@@ -0,0 +1,26 @@
+#' Generate Emergence Group Movement Gifs
+#' @description
+#' Generate the figures and stitch together a GIF to evaluate emergence group
+#' movement over time, generally aggregated as cumulative per month
+#' @param emergence_group str: designation of the emergence group to review
+#' @param pos tibble: positives data set
+#' @param dist sf: shapefile of all districts
+#' @param ctry sf: shapefile of all countries
+#' @param out_file str: location where gif should be saved
+#' @returns GIF written out to a location
+create_emergence_group_gif <- function(
+  emergence_group,
+  pos,
+  dist,
+  ctry,
+  out_file
+  ){
+
+  data <- sirfunctions::get_all_polio_data()
+  pos <- data$pos
+  emergence_group <- "NIE-JIS-1"
+  dist <- data$global.dist
+  ctry <- data$global.ctry
+  out_file <- "C:/Users/ynm2/OneDrive - CDC/NIE_JIS_1.gif"
+
+}
