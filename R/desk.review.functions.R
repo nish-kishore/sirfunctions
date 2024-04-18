912d13932c9c7cfa43acaa01c259fefac4f02f9f
mcuadera <xrg9@cdc.gov>
2024-04-18 11:13:11 -0400

    updated init_dr

--- R/desk.review.functions.R
+++ R/desk.review.functions.R
@@ -1,14 +1,65 @@
 # "private" methods ----
 
+#' Sets up local folders in the current working directory for desk review
+#' data_path is expected to be ~/country/year
+#'
+#' @param data_path `str` path where folders are to be created
+#'
+set_dr_local_folders <- function(path) {
+  # Check if required directories exist locally and create it if it does not
+  if (!dir.exists(path)) {
+    dir.create(path, recursive = T)
+  }
+
+  # Check if a country folder already exists and create it if it does not
+  local_country_path <- file.path(path)
+  if (!dir.exists(local_country_path)) {
+    dir.create(local_country_path, recursive = T)
+  }
+
+  # Check if a country folder contains data and metadata folders and create it if
+  # it does not
+  data_dir_path <- file.path(local_country_path, "data")
+  metadata_dir_path <- file.path(local_country_path, "metadata")
+  fig_dir_path <- file.path(local_country_path, "figures")
+  error_dir_path <- file.path(local_country_path, "errors")
+  param_dir_path <- file.path(local_country_path, "paramaters")
+
+  for (i in c(data_dir_path, metadata_dir_path, fig_dir_path, error_dir_path,
+              param_dir_path)) {
+    if (!dir.exists(i)) {
+      dir.create(i, recursive = T)
+    }
+  }
+  message(paste0("Successfully created local folder for desk review at:\n", path))
+  return(path)
+}
+
+#' Determine the size of data to pull
+#'
+#' @param year `int` start year of the desk review
+#'
+#' @return `str` size of the data
+set_data_size <- function(year) {
+  if (year >= 2019) {
+    "small"
+  } else if (year >= 2016) {
+    "medium"
+  } else {
+    "large"
+  }
+}
+
 #' Pull new data and write to the specified file path
 #'
 #' @param data_size `str` size of polio data to pull.
 #' "small" (>= 2019), "medium" (>= 2016), large" (full)
 #' @param country_name `str` name of the country to pull data from
-#' @param dr_data_path `str` path to save the data set to`
+#' @param dr_data_path `str` path to save the data set to.
+#' Expected path is ./country/year/data
 #'
 #' @return `list` large list containing polio data
-copy_new_data <- function(data_size, country_name, dr_data_path) {
+update_data <- function(data_size, country_name, dr_data_path, attach_spatial_data) {
   message("Saving a new copy of the dataset.")
   path_to_save <- paste0(paste(dr_data_path, "saved_on", Sys.Date(), sep = "_"), ".Rds")
 
@@ -17,28 +68,11 @@ copy_new_data <- function(data_size, country_name, dr_data_path) {
     file.remove(path_to_save)
   }
 
-  raw_data <- sirfunctions::get_all_polio_data(size = data_size)
-  country_data <- sirfunctions::extract_country_data(country_name, raw_data)
+  raw_data <- get_all_polio_data(size = data_size, attach.spatial.data = attach_spatial_data)
+  country_data <- extract_country_data(country_name, raw_data)
   readr::write_rds(country_data, path_to_save)
   message(paste0("Data saved at:\n", dr_data_path))
   return(country_data)
-}
-
-#' Creates a meta data file
-#'
-#' @param path location where the .txt file is located/should be created
-#'
-#' @return this function does not return anything
-create_metadata <- function(path) {
-  date_updated <- paste("Updated:", Sys.time(), sep = " ")
-  file_location <- paste("Project working directory:", getwd(), sep = " ")
-
-  if (!exists(path)) {
-    file.create(path)
-  }
-
-  write_lines(file = path,
-              x = c(date_updated, file_location))
 }
 
 #' Loads .Rds file
@@ -71,72 +105,45 @@ load_data <- function(data_dir_path) {
   }
 }
 
+#' Creates a meta data file
+#'
+#' @param path location where the .txt file is located/should be created
+#'
+#' @return this function does not return anything
+create_metadata <- function(path) {
+  date_updated <- paste("Updated:", Sys.time(), sep = " ")
+  file_location <- paste("Project working directory:", getwd(), sep = " ")
 
-#' Determine the size of data to pull
-#'
-#' @param year `int` start year of the desk review
-#'
-#' @return `str` size of the data
-set_data_size <- function(year) {
-  if (year >= 2019) {
-    "small"
-  } else if (year >= 2016) {
-    "medium"
-  } else {
-    "large"
+  if (!exists(path)) {
+    file.create(path)
   }
+
+  write_lines(file = path,
+              x = c(date_updated, file_location))
 }
 
-#' Sets up local folders in the current working directory for storing data and metadata
-#'
-#' @param data_path `str` path where data is/to be stored
-#' @param country_name `str` name of the country
-#'
-#' @return `str` returns the path containing data for the country
-setup_local_folders <- function(data_path, country_name) {
-  # Check if required directories exist locally and create it if it does not
-  if (!dir.exists(data_path)) {
-    dir.create(data_path, recursive = T)
-  }
-
-  # Check if a country folder already exists and create it if it does not
-  local_country_path <- file.path(data_path, country_name)
-  if (!dir.exists(local_country_path)) {
-    dir.create(local_country_path, recursive = T)
-  }
-
-  # Check if a country folder contains data and metadata folders and create it if
-  # it does not
-  data_dir_path <- file.path(local_country_path, "data")
-  metadata_dir_path <- file.path(local_country_path, "metadata")
-  if (!dir.exists(data_dir_path)) {
-    dir.create(data_dir_path, recursive = T)
-  }
-  if (!dir.exists(metadata_dir_path)) {
-    dir.create(metadata_dir_path, recursive = T)
-  }
-  return(local_country_path)
+freeze_data <- function() {
+  return(0)
 }
-
 
 #' Handles the logic of which files to load in the current R session
 #'
-#' @param data_dir_path `str` path to the dataset
+#' @param dr_data_path `str` path to the dataset
 #' @param data_size `str` "small", "medium", or "large"
 #' @param country_name `str` name of the country
 #' @param dr_data_path `str` path to save the dataset to
 #'
 #' @return `list` large list containing polio data
-initiate_research_logic <- function(data_dir_path, data_size, country_name, dr_data_path) {
+generate_data <- function(data_path, data_size, country_name, dr_data_path, attach_spatial_data) {
 
-  data_exists <- length(list.files(data_dir_path)) != 0
+  data_exists <- length(list.files(data_path)) != 0
   if (data_exists) {
     response <- T
     while (response) {
       cli::cli_alert_info("Previous save(s) found:")
 
-      for (i in 1:length(list.files(data_dir_path))) {
-        message(paste0(i, ") ", list.files(data_dir_path)[i]))
+      for (i in 1:length(list.files(data_path))) {
+        message(paste0(i, ") ", list.files(data_path)[i]))
       }
 
       message("Save a new copy? Type 'y','n', or 'q' to quit.")
@@ -144,10 +151,10 @@ initiate_research_logic <- function(data_dir_path, data_size, country_name, dr_d
       create_new_save <- stringr::str_trim(stringr::str_to_lower(create_new_save))
 
       if (create_new_save == "y") {
-        message(paste0("Data saving at:\n", data_dir_path))
-        return(copy_new_data(data_size, country_name, dr_data_path))
+        message(paste0("Data saving at:\n", dr_data_path))
+        return(update_data(data_size, country_name, dr_data_path, attach_spatial_data))
       } else if (create_new_save == "n") {
-        return(load_data(data_dir_path))
+        return(load_data(data_path))
       } else if (create_new_save == "q") {
         message("Aborting dataset loading...")
         response <- F
@@ -156,8 +163,8 @@ initiate_research_logic <- function(data_dir_path, data_size, country_name, dr_d
       }
     }
   } else {
-    message(paste0("Data saved at:\n", data_dir_path))
-    return(copy_new_data(data_size, country_name, dr_data_path))
+    message(paste0("Data saved at:\n", dr_data_path))
+    return(update_data(data_size, country_name, dr_data_path))
   }
 }
 
@@ -170,42 +177,39 @@ initiate_research_logic <- function(data_dir_path, data_size, country_name, dr_d
 #'
 #' @return `list` large list containing all dataframe for all polio data
 #' @export
-initiate_research <- function(country_name, start_date, end_date) {
+init_dr <- function(country_name, start_date, end_date, local_dr_repo, sg_dr_repo, attach_spatial_data) {
   country_name <- stringr::str_trim(stringr::str_to_upper(country_name))
   start_date <- lubridate::as_date(start_date)
   end_date <- lubridate::as_date(end_date)
+  year <- lubridate::year(start_date)
 
   # Set up local directory for storing for data and metadata
-  df_name <- stringr::str_c(country_name, start_date, end_date, sep = "_")
+  df_name <- stringr::str_c(stringr::str_to_lower(country_name), start_date, end_date, sep = "_")
 
   # Relative path of where data and metadata is stored
-  data_path <- file.path("data", "countries")
+  dr_path <- file.path(local_dr_repo, stringr::str_to_lower(country_name), year)
 
   # Set up local folders
-  country_dir_path <- setup_local_folders(data_path, country_name)
+  country_dir_path <- set_dr_local_folders(dr_path)
 
   # Create a local copy of the dataset in the folder, or update it.
   dr_data_path <- file.path(country_dir_path, "data", df_name)
 
   # Determine the file size of the data to be downloaded
-  start_date_year <- lubridate::year(start_date)
-  data_size <- set_data_size(start_date_year)
+  data_size <- set_data_size(year)
 
   # Create meta data
-  metadata_path <- file.path(country_dir_path,
+  metadata_path <- file.path(dr_path,
                              "metadata",
                              paste0(paste(df_name, "metadata", sep = "_"), ".txt"))
   create_metadata(file.path(metadata_path))
 
   # Instantiate variable containing country data and desk review meta data
   data_path <- file.path(country_dir_path, "data")
-  country_data <- initiate_research_logic(
+  country_data <- generate_data(
     data_path, data_size, country_name,
-    dr_data_path
+    dr_data_path, attach_spatial_data
   )
   return(country_data)
 }
 
-initiate_desk_review <- function(country_name, start_date, end_date) {
-  return(0)
-}
