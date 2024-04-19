8f803e0d26ee4a136d2df709e2866b94ef876f21
mcuadera <xrg9@cdc.gov>
2024-04-19 10:12:59 -0400

    added guids for prov and dist in extract_country_data

--- R/dal.R
+++ R/dal.R
@@ -1059,6 +1059,8 @@ extract_country_data <- function(
            prov = ADM1_NAME,
            dist = ADM2_NAME,
            u15pop,
+           adm0guid,
+           adm1guid,
            adm2guid,
            datasource)
 
@@ -1077,6 +1079,7 @@ extract_country_data <- function(
                   ctry = ADM0_NAME,
                   prov = ADM1_NAME,
                   u15pop = u15pop.prov,
+                  adm0guid,
                   adm1guid,
                   datasource)
 
