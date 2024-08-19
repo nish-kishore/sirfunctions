bb5c6af07fb7fd828ece28b5193a2105ada74e09
Nick Heaghney <103440775+Heaghneyn@users.noreply.github.com>
2024-08-19 17:09:21 -0400

    simplifying clean case data and applying correctly

--- R/sia.breakthrough.defs.R
+++ R/sia.breakthrough.defs.R
@@ -13,19 +13,16 @@ raw.data <- get_all_polio_data()
 #remove wild 1 in afg + pak
 case.list <- c("cVDPV 1", "cVDPV 2", "cVDPV 3", "WILD 1")
 
-list.cases <- lapply(case.list,
-                     FUN = clean_case_data,
-                     sia.data = raw.data$sia,
-                     start.date = as.Date("2000-01-01"),
-                     end.date = Sys.Date(), min.yronset = paste0(year(Sys.Date())-4),
-                     type = "reg")
+cvdpv.wild.cases <- clean_case_data(case.data = raw.data$pos,
+                              .measurement = case.list,
+                              start.date = as.Date("2000-01-01"),
+                              end.date = Sys.Date(),
+                              type = "reg") |>
+  dplyr::mutate(place.admin.1=ifelse(place.admin.1=="KHYBER PAKHTOON", "KP", place.admin.1),
+                place.admin.1=ifelse(place.admin.1=="KPTD", "KP", place.admin.1),
+                place.admin.1=ifelse(place.admin.1=="KPAKHTUNKHWA", "KP", place.admin.1),
+                place.admin.1=ifelse(place.admin.1=="GBALTISTAN", "GB", place.admin.1),
+                place.admin.1=ifelse(place.admin.1=="GILGIT BALTISTAN", "GB", place.admin.1)) |>
+  dplyr::filter(!(place.admin.0 %in% c("AFGHANISTAN", "PAKISTAN") & measurement == "WILD 1"))
 
-cvdpv.wild.cases <- do.call(rbind.data.frame, list.cases) %>%
-  #adding a hard fix to SIA data to fix Pakistan names to match virus
-  #place admin1 names now GB and KP to match EOC names
-  mutate(place.admin.1=ifelse(place.admin.1=="KHYBER PAKHTOON", "KP", place.admin.1),
-         place.admin.1=ifelse(place.admin.1=="KPTD", "KP", place.admin.1),
-         place.admin.1=ifelse(place.admin.1=="KPAKHTUNKHWA", "KP", place.admin.1),
-         place.admin.1=ifelse(place.admin.1=="GBALTISTAN", "GB", place.admin.1),
-         place.admin.1=ifelse(place.admin.1=="GILGIT BALTISTAN", "GB", place.admin.1)) %>%
-  filter(!(place.admin.0 %in% c("AFGHANISTAN", "PAKISTAN") & measurement == "WILD 1"))
+
