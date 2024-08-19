eda399fb42fa2170617b0fa830d8890a04cbd419
Nick Heaghney <103440775+Heaghneyn@users.noreply.github.com>
2024-08-19 15:21:35 -0400

    bringing in sia data pull from original report

--- R/sia.failure.functions.R
+++ R/sia.failure.functions.R
@@ -20,6 +20,363 @@ init_sia_impact <- function(folder_loc){
 
 }
 
+#### Pull Clean Data ####
+#' @description function to pull clean, de-duplicated SIA campaign data for different purposes within
+#' the SIA impact report
+#' @param start.date date, start date of SIA campaigns
+#' @param end.date date, system date = today, last date of SIA campaigns, should be day of analysis
+#' @param method chr, signifies type of SIA being pulled, reg includes n/mopv2, topv and bopv, planned
+#' is limited to upcoming SIAs, IPV is for countries/regions that will only do IPV response
+pull_clean_sia_data <- function(start.date=as.Date("2016-01-01"),
+                                end.date = Sys.Date(),
+                                method = 'reg'){
+  if(method == "reg"){
+    print("----BEGINNING SIA DATA PULL / CLEANING----")
+    tick <- Sys.time()
+    # read in SIA files
+    sia.clean.01 <- raw.data$sia |>
+      filter(activity.start.date >= start.date & activity.end.date<=end.date)
+
+    tock <- Sys.time()
+
+    print(paste0("Data loaded successfully!"))
+    print(paste0(nrow(sia.clean.01), " records loaded in ", as.numeric(tock-tick),
+                 " seconds."))
+
+    sia.02 <- sia.clean.01 %>%
+      #select variables of interest to ease error checking
+      select(sia.sub.activity.code,
+             status, phase, im.loaded, lqas.loaded, vaccine.type,
+             sub.activity.start.date, adm2guid,
+             linked.obx,
+             # activity.comments,
+             yr.sia, place.admin.0,
+             place.admin.1, place.admin.2,sub.activity.end.date,
+             vaccine.type, adm0guid, adm1guid, `admin.coverage.%`)
+
+    print("[0/3]-Starting cleaning steps")
+
+    print("[1/3]-Removing campaigns that did not occur")
+    tick <- Sys.time()
+
+    sia.02 <- sia.02 %>%
+      #first step is to get rid of campaigns that did not occur
+      #Unconfirmed was used pre-2014 for some older campaigns
+      #missing status for campaigns 2004-2017 will be assigned as unconfirmed\
+      mutate(
+        complete.camp = case_when(
+          status == "Done" |
+            phase == "Completed" |
+            im.loaded == "Yes" |
+            lqas.loaded == "Yes" ~ "Completed",
+          status == "Cancelled" |
+            status == "Delayed" |
+            (status == "Planned" & phase != "Completed") ~ "Not completed",
+          status == "Unconfirmed" ~"Unconfirmed"),
+        type.2.opv = ifelse(vaccine.type == "mOPV2" |
+                              vaccine.type=="nOPV2" |
+                              vaccine.type=="tOPV", 1, 0),
+        type.1.opv = ifelse(vaccine.type == "bOPV" |
+                              vaccine.type=="mOPV1" |
+                              vaccine.type=="tOPV"|
+                              vaccine.type=="IPV + bOPV", 1, 0),
+        type.3.opv = ifelse(vaccine.type=="mOPV3" |
+                              vaccine.type=="bOPV" |
+                              vaccine.type=="tOPV" |
+                              vaccine.type=="IPV + bOPV", 1, 0),
+        ipv.vac = ifelse(vaccine.type == "f-IPV" |
+                           vaccine.type == "IPV" |
+                           vaccine.type == "IPV + bOPV", 1, 0)) %>%
+      filter(complete.camp == "Completed" |
+               complete.camp == "Unconfirmed")
+
+    tock <- Sys.time()
+
+    print(tock - tick)
+
+    print("[2/3]-Removing duplicates")
+
+    tick <- Sys.time()
+
+    sia.02 <- sia.02 %>%
+
+      #Second step is to remove duplicates:
+
+      mutate(sub.activity.start.date = as_date(sub.activity.start.date)) %>%
+      arrange(sub.activity.start.date) %>%
+      group_by(adm2guid, vaccine.type) %>%
+      #this creates variable that is days from last campaign of that vaccine
+      mutate(camp.diff.days = as.numeric(sub.activity.start.date - lag(sub.activity.start.date)))%>%
+      ungroup() %>%
+
+      #identify SIAs that are duplicated because there are no difference in campaign days
+      mutate(dup = case_when(
+        camp.diff.days == 0 & !is.na(adm2guid) ~ 1,
+        camp.diff.days > 0 | is.na(adm2guid) == T | is.na(camp.diff.days) == T ~ 0)) %>%
+      #remove duplicates
+      filter(dup != 1) %>%
+      #manually removing extra duplicates
+      #same date, vaccine and age range as another campaign in same dist
+      filter(sia.sub.activity.code!="PAK-2021-006-1") %>%
+      #this one no IM
+      filter(sia.sub.activity.code!="SOM-2000-002-2")
+
+    tock <- Sys.time()
+
+    print(tock - tick)
+
+
+    print("[3/3]-Final cleaning steps")
+
+
+    sia.reg <- sia.02%>%
+      filter(vaccine.type=="mOPV2" | vaccine.type=="nOPV2" | (vaccine.type=="tOPV" & sub.activity.start.date>"2017-01-01") | vaccine.type == "bOPV") %>%
+      select(sia.sub.activity.code, place.admin.0, place.admin.1, place.admin.2, sub.activity.start.date, sub.activity.end.date, vaccine.type, adm0guid, adm1guid, adm2guid,
+             yr.sia,
+             # num.dist.incamp,
+             # vac.round.num.count,
+             # vac.total.rounds,
+             # opv2.round.count,
+             # total.opv2.rounds,
+             #linked.obx, obx1, obx2, obx3, obx4,
+             `admin.coverage.%`)
+
+    return(sia.reg)
+  }
+
+  if(method == "planned"){
+    print("----BEGINNING SIA DATA PULL / CLEANING----")
+    tick <- Sys.time()
+    # read in SIA files
+    sia.clean.01 <- raw.data$sia |>
+      filter(activity.start.date >= Sys.Date())
+
+    tock <- Sys.time()
+
+    print(paste0("Data loaded successfully!"))
+    print(paste0(nrow(sia.clean.01), " records loaded in ", as.numeric(tock-tick),
+                 " seconds."))
+
+    sia.02 <- sia.clean.01 %>%
+      #select variables of interest to ease error checking
+      select(sia.sub.activity.code,
+             status, phase, im.loaded, lqas.loaded, vaccine.type,
+             sub.activity.start.date, adm2guid,
+             linked.obx,
+             # activity.comments,
+             yr.sia, place.admin.0,
+             place.admin.1, place.admin.2,sub.activity.end.date,
+             vaccine.type, adm0guid, adm1guid, `admin.coverage.%`)
+
+    print("[0/3]-Starting cleaning steps")
+
+    print("[1/3]- subsetting to planned campaigns")
+    tick <- Sys.time()
+
+    sia.02 <- sia.02 %>%
+      #first step is to get rid of campaigns that did not occur
+      #Unconfirmed was used pre-2014 for some older campaigns
+      #missing status for campaigns 2004-2017 will be assigned as unconfirmed\
+      mutate(
+        planned.camp = ifelse(phase %in% c("Forecasted", "Planned") &
+                                status != "Canceled", "Planned", NA),
+        complete.camp = case_when(
+          status == "Done" |
+            phase == "Completed" |
+            im.loaded == "Yes" |
+            lqas.loaded == "Yes" ~ "Completed",
+          status == "Cancelled" |
+            status == "Delayed" |
+            (status == "Planned" & phase != "Completed") ~ "Not completed",
+          status == "Unconfirmed" ~"Unconfirmed"),
+        type.2.opv = ifelse(vaccine.type == "mOPV2" |
+                              vaccine.type=="nOPV2" |
+                              vaccine.type=="tOPV", 1, 0),
+        type.1.opv = ifelse(vaccine.type == "bOPV" |
+                              vaccine.type=="mOPV1" |
+                              vaccine.type=="tOPV"|
+                              vaccine.type=="IPV + bOPV", 1, 0),
+        type.3.opv = ifelse(vaccine.type=="mOPV3" |
+                              vaccine.type=="bOPV" |
+                              vaccine.type=="tOPV" |
+                              vaccine.type=="IPV + bOPV", 1, 0),
+        ipv.vac = ifelse(vaccine.type == "f-IPV" |
+                           vaccine.type == "IPV" |
+                           vaccine.type == "IPV + bOPV", 1, 0)) %>%
+      filter(planned.camp == "Planned" &
+               year(Sys.Date()) <= yr.sia)
+
+    tock <- Sys.time()
+
+    print(tock - tick)
+
+    print("[2/3]-Removing duplicates")
+
+    tick <- Sys.time()
+
+    sia.02 <- sia.02 %>%
+
+      #Second step is to remove duplicates:
+
+      mutate(sub.activity.start.date = as_date(sub.activity.start.date)) %>%
+      arrange(sub.activity.start.date) %>%
+      group_by(adm2guid, vaccine.type) %>%
+      #this creates variable that is days from last campaign of that vaccine
+      mutate(camp.diff.days = as.numeric(sub.activity.start.date - lag(sub.activity.start.date)))%>%
+      ungroup() %>%
+
+      #identify SIAs that are duplicated because there are no difference in campaign days
+      mutate(dup = case_when(
+        camp.diff.days == 0 & !is.na(adm2guid) ~ 1,
+        camp.diff.days > 0 | is.na(adm2guid) == T | is.na(camp.diff.days) == T ~ 0)) %>%
+      #remove duplicates
+      filter(dup != 1) %>%
+      #manually removing extra duplicates
+      #same date, vaccine and age range as another campaign in same dist
+      filter(sia.sub.activity.code!="PAK-2021-006-1") %>%
+      #this one no IM
+      filter(sia.sub.activity.code!="SOM-2000-002-2")
+
+    tock <- Sys.time()
+
+    print(tock - tick)
+
+
+    print("[3/3]-Final cleaning steps")
+
+
+    sia.planned <- sia.02 %>%
+      # filter(vaccine.type=="mOPV2" | vaccine.type=="nOPV2" | (vaccine.type=="tOPV" & sub.activity.start.date>"2017-01-01") | vaccine.type == "bOPV") %>%
+      select(sia.sub.activity.code, place.admin.0, place.admin.1, place.admin.2, sub.activity.start.date, sub.activity.end.date, vaccine.type, adm0guid, adm1guid, adm2guid,
+             yr.sia, status, phase,
+             # num.dist.incamp,
+             # vac.round.num.count,
+             # vac.total.rounds,
+             # opv2.round.count,
+             # total.opv2.rounds,
+             #linked.obx, obx1, obx2, obx3, obx4,
+             `admin.coverage.%`)
+
+
+    return(sia.planned)
+  }
+
+  if(method == "ipv"){
+    print("----BEGINNING SIA DATA PULL / CLEANING----")
+    tick <- Sys.time()
+    print("Connecting to S drive to pull latest data...")
+    # read in SIA files
+    sia.clean.01 <- raw.data$sia |>
+      filter(activity.start.date >= start.date)
+
+    tock <- Sys.time()
+
+    print(paste0("Data loaded successfully!"))
+    print(paste0(nrow(sia.clean.01), " records loaded in ", as.numeric(tock-tick),
+                 " seconds."))
+
+    sia.02 <- sia.clean.01 %>%
+      #select variables of interest to ease error checking
+      select(sia.sub.activity.code,
+             status, phase, im.loaded, lqas.loaded, vaccine.type,
+             sub.activity.start.date, adm2guid,
+             linked.obx,
+             # activity.comments,
+             yr.sia, place.admin.0,
+             place.admin.1, place.admin.2,sub.activity.end.date,
+             vaccine.type, adm0guid, adm1guid, `admin.coverage.%`)
+
+    print("[0/3]-Starting cleaning steps")
+
+    print("[1/3]-Removing campaigns that did not occur")
+    tick <- Sys.time()
+
+    sia.02 <- sia.02 %>%
+      #first step is to get rid of campaigns that did not occur
+      #Unconfirmed was used pre-2014 for some older campaigns
+      #missing status for campaigns 2004-2017 will be assigned as unconfirmed\
+      mutate(
+        complete.camp = case_when(
+          status == "Done" |
+            phase == "Completed" |
+            im.loaded == "Yes" |
+            lqas.loaded == "Yes" ~ "Completed",
+          status == "Cancelled" |
+            status == "Delayed" |
+            (status == "Planned" & phase != "Completed") ~ "Not completed",
+          status == "Unconfirmed" ~"Unconfirmed"),
+        type.2.opv = ifelse(vaccine.type == "mOPV2" |
+                              vaccine.type=="nOPV2" |
+                              vaccine.type=="tOPV", 1, 0),
+        type.1.opv = ifelse(vaccine.type == "bOPV" |
+                              vaccine.type=="mOPV1" |
+                              vaccine.type=="tOPV"|
+                              vaccine.type=="IPV + bOPV", 1, 0),
+        type.3.opv = ifelse(vaccine.type=="mOPV3" |
+                              vaccine.type=="bOPV" |
+                              vaccine.type=="tOPV" |
+                              vaccine.type=="IPV + bOPV", 1, 0),
+        ipv.vac = ifelse(vaccine.type == "f-IPV" |
+                           vaccine.type == "IPV" |
+                           vaccine.type == "IPV + bOPV", 1, 0)) %>%
+      filter(complete.camp == "Completed" |
+               complete.camp == "Unconfirmed")
+
+    tock <- Sys.time()
+
+    print(tock - tick)
+
+    print("[2/3]-Removing duplicates")
+
+    tick <- Sys.time()
+
+    sia.02 <- sia.02 %>%
+
+      #Second step is to remove duplicates:
+
+      mutate(sub.activity.start.date = as_date(sub.activity.start.date)) %>%
+      arrange(sub.activity.start.date) %>%
+      group_by(adm2guid, vaccine.type) %>%
+      #this creates variable that is days from last campaign of that vaccine
+      mutate(camp.diff.days = as.numeric(sub.activity.start.date - lag(sub.activity.start.date)))%>%
+      ungroup() %>%
+
+      #identify SIAs that are duplicated because there are no difference in campaign days
+      mutate(dup = case_when(
+        camp.diff.days == 0 & !is.na(adm2guid) ~ 1,
+        camp.diff.days > 0 | is.na(adm2guid) == T | is.na(camp.diff.days) == T ~ 0)) %>%
+      #remove duplicates
+      filter(dup != 1) %>%
+      #manually removing extra duplicates
+      #same date, vaccine and age range as another campaign in same dist
+      filter(sia.sub.activity.code!="PAK-2021-006-1") %>%
+      #this one no IM
+      filter(sia.sub.activity.code!="SOM-2000-002-2")
+
+    tock <- Sys.time()
+
+    print(tock - tick)
+
+
+    print("[3/3]-Final cleaning steps")
+
+
+    sia.ipv <- sia.02%>%
+      filter(vaccine.type=="IPV" | vaccine.type=="f-IPV" | vaccine.type=="IPV + bOPV") %>%
+      select(sia.sub.activity.code, place.admin.0, place.admin.1, place.admin.2, sub.activity.start.date, sub.activity.end.date, vaccine.type, adm0guid, adm1guid, adm2guid,
+             yr.sia,
+             # num.dist.incamp,
+             # vac.round.num.count,
+             # vac.total.rounds,
+             # opv2.round.count,
+             # total.opv2.rounds,
+             #linked.obx, obx1, obx2, obx3, obx4,
+             `admin.coverage.%`)
+
+    return(sia.ipv)
+  }
+}
+
 
 #Cluster Function
 #this function identifies "cluster" or OBX response so we can identify rounds
