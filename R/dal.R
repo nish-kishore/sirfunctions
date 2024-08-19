00d4e23992782c2ff2c9301b406e93ca42587c97
mcuadera <xrg9@cdc.gov>
2024-08-19 12:02:33 -0400

    run check()

--- R/dal.R
+++ R/dal.R
@@ -481,14 +481,14 @@ get_all_polio_data <- function(
         hot.case = ifelse(is.na(hot.case), 99, hot.case)
       )
 
-    if(nrow(raw.data$afp[duplicated(raw.data$afp[,c("epid", "place.admin.0", "dateonset")]),]) > 0){
+    if (nrow(raw.data$afp[duplicated(raw.data$afp[,c("epid", "place.admin.0", "dateonset")]),]) > 0) {
       cli::cli_alert_warning("There are potential duplicates in the AFP linelist, please check afp.dup")
       raw.data$afp.dup <- raw.data$afp |>
-        dplyr::group_by(epid, place.admin.0, dateonset) |>
+        dplyr::group_by(.data$epid, .data$place.admin.0, .data$dateonset) |>
         dplyr::mutate(count = dplyr::n()) |>
         dplyr::ungroup() |>
         dplyr::filter(count > 1) |>
-        dplyr::arrange(epid)
+        dplyr::arrange(.data$epid)
 
     }
 
