0c326a70578eef0a310f3c6aff09e0561820a994
Nick Heaghney <103440775+Heaghneyn@users.noreply.github.com>
2024-08-20 14:42:41 -0400

    update packages and pipes and imports

--- R/sia.failure.functions.R
+++ R/sia.failure.functions.R
@@ -339,6 +339,7 @@ clean_case_data <- function(case.data,
 #Cluster Function
 #this function identifies "cluster" or OBX response so we can identify rounds
 #' @export
+#' @import dplyr stats
 #' @param x df: data to be clustered
 #' @param seed num
 #' @param method str cluster method to use, can be "kmeans" or "mindate"
@@ -350,33 +351,33 @@ cluster_dates <- function(x,
 
   if(method == "kmeans"){
     #prepare the data
-    y <- x %>%
+    y <- x |>
       #select only dates
-      select(date = sub.activity.start.date) %>%
+      dplyr::select(date = sub.activity.start.date) |>
       #calculate distance from minimum date
-      mutate(date = as.numeric(date - min(date))) %>%
+      dplyr::mutate(date = as.numeric(date - min(date))) |>
       #normalize values for clustering
       scale()
 
     set.seed(seed)
     #calculate the optimal number of clusters
-    optim_k <- y %>%
+    optim_k <- y |>
       #calculate optimal number of clusters using the
       #gap statistic
-      {clusGap(., FUN = kmeans, nstart = 25, K.max = max(min(nrow(.)-1, nrow(.)/2), 2), B = 100)} %>%
+      {clusGap(., FUN = stats::kmeans, nstart = 25, K.max = max(min(nrow(.)-1, nrow(.)/2), 2), B = 100)} |>
       #extract gap statistic matrix
-      {.$Tab[,"gap"]} %>%
+      {.$Tab[,"gap"]} |>
       #calculate the max gap statistic, given the sparsity in the data
       #am not limiting to the first max SE method
       which.max()
 
     set.seed(seed)
     #calculate the clusters
-    x$cluster <- kmeans(y, optim_k)$cluster %>%
+    x$cluster <- stats::kmeans(y, optim_k)$cluster |>
       #clusters don't always come out in the order we want them to
       #so here we convert them into factors, relevel and then extract
       #the numeric value to ensure that the cluster numbers are in order
-      {factor(., levels = unique(.))} %>%
+      {factor(., levels = unique(.))} |>
       as.numeric()
 
     #outputting the method used
@@ -385,11 +386,11 @@ cluster_dates <- function(x,
     return(x)
   }else{
     if(method == "mindate"){
-      x <- x %>%
-        mutate(cluster = as.numeric(sub.activity.start.date - min(sub.activity.start.date)),
-               cluster = cut(cluster, seq(0, grouping_days*6, by = grouping_days), include.lowest = T),
-               cluster = as.numeric(cluster),
-               cluster_method = method)
+      x <- x |>
+        dplyr::mutate(cluster = as.numeric(sub.activity.start.date - min(sub.activity.start.date)),
+                      cluster = cut(cluster, seq(0, grouping_days*6, by = grouping_days), include.lowest = T),
+                      cluster = as.numeric(cluster),
+                      cluster_method = method)
 
       return(x)
     }
@@ -401,6 +402,8 @@ cluster_dates <- function(x,
 
 #' @description
 #' manager function to run the cluster_dates() function using helper function run_cluster_dates to cluster SIAs by type
+#' @export
+#' @import package
 #' @param df dataframe of SIAs to identify rounds by vaccine type
 cluster_dates_for_sias <- function(sia.type2){
 
